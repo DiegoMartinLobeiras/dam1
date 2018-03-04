1d72ad7086ca05b07163967ee3eccc6e1b8dafa2
snk_d <snk_d@DESKTOP-C4MOIVM>
2018-03-04 20:27:59 +0100

    Preba git

---
+++ git/src/git/Git.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author Diego Martín <your.name at your.org>
+ */
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Diego";
+        int años = 19;
+        System.out.println("Hola Mundo, me llamo "+nombre+" y tengo "+años+" años");
+    }
+    
+}
